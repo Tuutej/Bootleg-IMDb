5ba8e3951ad8c70abb97cb152f770ba2eed0d7d4
Tuutej <90647045+Tuutej@users.noreply.github.com>
2023-11-15 16:24:22 +0200

    Update MovieRestController.java

--- src/main/java/hh/sof03/bootlegimdb/web/MovieRestController.java
+++ src/main/java/hh/sof03/bootlegimdb/web/MovieRestController.java
@@ -25,36 +25,35 @@ public class MovieRestController {
     // RESTful service to list all movies
     @RequestMapping(value = "/movies", method = RequestMethod.GET)
     public @ResponseBody List<Movie> movieListRest() {
-        // Returns a list of all movies from the repository
+    
         return (List<Movie>) movieRepository.findAll();
     }
 
     // RESTful service to get a movie by ID
     @RequestMapping(value = "/movies/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Movie> findMovieRest(@PathVariable("id") Long movieId) {
-        // Retrieves a movie by its ID from the repository
+      
         return movieRepository.findById(movieId);
     }
     
     // RESTful service to create a new movie
     @RequestMapping(value = "/movies", method = RequestMethod.POST)
     public @ResponseBody Movie createMovie(@RequestBody Movie movie) {
-        // Saves a new movie to the repository and returns the saved movie
+       
         return movieRepository.save(movie);
     }
 
     // RESTful service to update a movie by ID
     @RequestMapping(value = "/movies/{id}", method = RequestMethod.PUT)
     public @ResponseBody Movie updateMovie(@PathVariable("id") Long movieId, @RequestBody Movie updatedMovie) {
-        // Retrieves an existing movie by its ID, updates its details, and saves it to the repository
+       
         Optional<Movie> optionalMovie = movieRepository.findById(movieId);
         if (optionalMovie.isPresent()) {
             Movie movie = optionalMovie.get();
-            // Update movie details here using updatedMovie
-            // ...
+           
             return movieRepository.save(movie);
         } else {
-            // Handle scenario when movie is not found
+           
             return null;
         }
     }
@@ -62,7 +61,7 @@ public class MovieRestController {
     // RESTful service to delete a movie by ID
     @RequestMapping(value = "/movies/{id}", method = RequestMethod.DELETE)
     public void deleteMovie(@PathVariable("id") Long movieId) {
-        // Deletes a movie from the repository by its ID
+       
         movieRepository.deleteById(movieId);
     }
 }
